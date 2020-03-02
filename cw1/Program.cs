df2aed93b539a3aefbc0cccc47288fdc2fd01728
s18579 <s18579@LAB001-13>
2020-03-02 09:56:28 +0100

    Pierwszy commit

---
+++ cw1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //string a = "e";
+            //string b = "lo";
+            //Console.WriteLine($"{a}{b}"); // concat
+            //string path = @"https://github.com/s18579/cw1.git";
+            //Console.WriteLine("Hello World!");
+            //var newPerson = new Person { FirstName = "Jacek" };
+            var url = "https://www.pja.edu.pl";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                //2xx
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(htmlContent);
+                foreach(var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+        }
+    }
+}
